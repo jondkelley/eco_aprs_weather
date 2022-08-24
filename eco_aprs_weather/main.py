65d9f8fee810a1474b040f61284752b545200b51
toor <jonk@omg.lol>
2022-08-23 22:34:19 -0400

    X

--- eco_aprs_weather/main.py
+++ eco_aprs_weather/main.py
@@ -305,9 +305,9 @@ def metrics(metric):
             dt = datetime.datetime.strptime(dtstr, '%Y-%m-%d+%H:%M')
             epoch = calendar.timegm(dt.timetuple())
             try:
-                output.append([epoch float(item[metric])])
+                output.append([epoch, float(item[metric])])
             except ValueError:
-                output.append([epoch float(item[metric])])
+                output.append([epoch, float(item[metric])])
     return jsonify(output)
 
 def main():
