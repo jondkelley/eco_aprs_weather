d73fcf7432050d54fc06b8e3d6d714b4e0c264a2
toor <jonk@omg.lol>
2022-08-26 15:48:06 -0400

    add sensor report webpage

--- eco_aprs_weather/main.py
+++ eco_aprs_weather/main.py
@@ -239,16 +239,22 @@ def update_hourlyrainfall_into_memory(post_dict):
    utcday = post_dict['dateutc'].split('+')[0]
    print(wx.hourlyrainfall)
    if utcday in wx.hourlyrainfall:
-      wx.hourlyrainfall[utcday][utchour] = post_dict['hourlyrainin']
+      try:
+          wx.hourlyrainfall[utcday][utchour] = post_dict['hourlyrainin']
+      except:
+          wx.hourlyrainfall[utcday][utchour] = 0
    else:
-      wx.hourlyrainfall[utcday] = {}
-      wx.hourlyrainfall[utcday][utchour] = post_dict['hourlyrainin']
+      try:
+          wx.hourlyrainfall[utcday] = {}
+          wx.hourlyrainfall[utcday][utchour] = post_dict['hourlyrainin']
+      except:
+          wx.hourlyrainfall[utcday][utchour] = 0
 
 @app.route('/data/report/', methods=['POST'])
 @app.route('/data/report', methods=['POST'])
 def weather_report():
     """ write WX report into memory from GW1004, GW1100B and similar devices """
-    # example of what device sends: curl -d "PASSKEY=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&stationtype=GW1000B_V1.5.5&dateutc=2020-01-05+02:25:46&tempinf=73.9&humidityin=42&baromrelin=2humidity1=37&temp2f=46.58&humidity2=49&soilmoisture1=93&soilmoisture2=52&batt1=0&batt2=0&soilbatt1=1.8&soilbatt2=1.7&freq=915M&model=GW1000_Pro" -H "Content-Type: application/x-wwwlhost:5001/data/report
+    # example of what device sends: curl -d "PASSKEY=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX&stationtype=GW1000B_V1.5.5&dateutc=2020-01-05+02:25:46&tempinf=73.9&humidityin=42&baromrelin=2humidity1=37&temp2f=46.58&humidity2=49&soilmoisture1=93&soilmoisture2=52&batt1=0&batt2=0&soilbatt1=1.8&soilbatt2=1.7&freq=915M&model=GW1000_Pro" localhost:5001/data/report
     if request.get_data().decode("utf-8") == '':
         print('/data/report recieved a zero payload input')
         return "Must send a POST payload", 400
@@ -299,16 +305,41 @@ def about():
 @app.route('/graph/metric/<metric>', methods=['GET'])
 def metrics(metric):
     output = list()
+    xdata = []
+    ydata = []
     for day, v in wx.metrics.items():
         for hour, item in v.items():
             dtstr = f'{day}+{hour}'
             dt = datetime.datetime.strptime(dtstr, '%Y-%m-%d+%H:%M')
             epoch = calendar.timegm(dt.timetuple())
             try:
-                output.append([epoch, float(item[metric])])
+                #output.append([epoch, float(item[metric])])
+                xdata.append(epoch)
+                ydata.append(float(item[metric]))
             except ValueError:
-                output.append([epoch, float(item[metric])])
-    return jsonify(output)
+                #output.append([epoch, float(item[metric])])
+                xdata.append(epoch)
+                ydata.append(float(item[metric]))
+
+    from nvd3 import cumulativeLineChart
+    chart = cumulativeLineChart(name='cumulativeLineChart', x_is_date=True)
+    #xdata = [1365026400000, 1365026500000, 1365026600000]
+    #ydata = [6, 5, 1]
+    #y2data = [36, 55, 11]
+
+    extra_serie = {"tooltip": {"y_start": "There are ", "y_end": " calls"}}
+    chart.add_serie(name="Serie 1", y=ydata, x=xdata, extra=extra_serie)
+
+    #extra_serie = {"tooltip": {"y_start": "", "y_end": " mins"}}
+    #chart.add_serie(name="Serie 2", y=y2data, x=xdata, extra=extra_serie)
+    chart.buildhtml()
+    return chart
+    #return jsonify(output)
+
+pairs_list = []
+@app.route('/sensor/overview')
+def index():
+    return render_template('sensor_overview.html', weather=singleton.weather, title="Sensor Overview")
 
 def main():
     app.run(host='0.0.0.0', port=5000)
