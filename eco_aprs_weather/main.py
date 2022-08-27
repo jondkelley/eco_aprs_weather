eb3497816896fb8e28771181838476ed24846820
toor <jonk@omg.lol>
2022-08-27 02:53:34 -0400

    add dynamic telem message update

--- eco_aprs_weather/main.py
+++ eco_aprs_weather/main.py
@@ -313,14 +313,14 @@ def set_telem_message():
     """will dynamically update the telemetry message broadcasted with wx beacon packet telemetry """
     args = request.args
     new_message = args.get("message", default=None, type=str)
-    no_flush = args.get("no_flush", default=None, type=str)
+    temporary = args.get("temporary", default=None, type=str)
     if not new_message:
       return 'error: must define `message` parameter in HTTP get request'
     configuration.status = new_message
 
     config['General']['telemetry_message'] = new_message
-    if not no_flush:
-      # UNLESS no flush is defined in request, flush the config to disk too!
+    if not temporary:
+      # UNLESS temporary is defined in request, flush the config to disk too!
       with open('/etc/bridge.ini', 'w') as configfile:    # save
           config.write(configfile)
     return 'update OK'
